27d0e8a8238e363dde0a79527d79caeae3ae7005
马扬 <mayang_email@sina.cn>
2019-10-09 13:35:46 +0800

    banner

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "MMSwiftPackage",
-    platforms: [.iOS(.v10)]
+    platforms: [.iOS(.v10)],
     products: [
         .library(
             name: "BannerView",
